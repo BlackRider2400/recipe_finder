8c1af0168e1ff17bb87ecedf9b5ed2b9d357423e
BlackRider2400 <krzysztof@baralkiewicz.pl>
2025-05-14 02:26:29 +0200

    fixed and deployed

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  base: "/recipe_finder/",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Recipe Finder",
+        short_name: "Recipes",
+        description: "Find and favorite your favorite meals!",
+        theme_color: "#ffcc00",
+        background_color: "#121212",
+        display: "standalone",
+        start_url: "/recipe_finder/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
